7dbca96b097a74168adb23c5189facc6648bd6a3
too519 <jintea519@gmail.com>
2022-11-01 17:18:24 +0900

    first commit

---
+++ src/main/java/com/ssafy/enjoytrip/EnjoyTripSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.enjoytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyTripSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyTripSpringBootApplication.class, args);
+	}
+
+}
